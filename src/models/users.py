a4e8e03d4ee27177612b3b880535014018606c7a
Samarth <samarth38work@gmail.com>
2025-02-25 14:20:55 +0000

    model upgrades

--- src/models/users.py
+++ src/models/users.py
@@ -9,10 +9,20 @@ class User(BaseModel):
     name: str
     email: EmailStr
     password: str
-    
+
     
     @validator('password')
     def validate_and_hash_password(cls, password: str) -> str:
+        """
+        Validates the password and hashes it if not already hashed
+
+        Requirements for password:
+            - At least 6 characters long
+            - At least one digit
+            - At least one uppercase letter
+            - At least one lowercase letter
+            - At least one special character        
+        """
         if len(password) < 6:
             raise ValueError("Password must be at least 6 characters long")
         if not any(char.isdigit() for char in password):
@@ -23,13 +33,20 @@ class User(BaseModel):
             raise ValueError("Password must contain at least one lowercase letter")
         if not any(char in "!@#$%^&*()-+" for char in password):
             raise ValueError("Password must contain at least one special character")
-        
+
         if PasswordHandle().is_password_hashed(password):
             return password
         return PasswordHandle().hashpassword(password)
-    
+
     class Config:
         allow_population_by_field_name = True
         json_encoders = {
             ObjectId: lambda x: str(x)
         }
+
+    def dict(self, *args, **kwargs):
+        """Overrides the dict method to remove the password field"""
+        user_dict = super().dict(*args, **kwargs)
+        if "_id" not in user_dict and "id" in user_dict:
+            user_dict["_id"] = user_dict.pop("id")
+        return user_dict
